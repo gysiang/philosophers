33eca37d61b3571edd0800cf50713db1dccf36f4
gyong-si <gyong-si@gyong-si.gyong-si.virtualbox.org>
2024-03-07 23:19:57 +0800

    start init data

--- includes/philo.h
+++ includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 12:00:46 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/03/06 16:23:58 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/03/07 23:13:38 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,29 @@
 # define THINKING "is thinking"
 # define DIED "died"
 
+typedef struct s_data
+{
+	int		num;
+	int		num_of_meals;
+	size_t	die_time;
+	size_t	eat_time
+	size_t	sleep_time;
+	size_t	start_time;
+
+} t_data;
+
 typedef struct s_philo
 {
-	bool	status;
+	int				id;
+	int				meals_eaten;
+	pthread_mutex_t	last_meal_mutex;
+	size_t			last_meal_time;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 } t_philo;
 
 int	ft_atoi(const char *str);
 int	is_number(const char *s);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
--- philo/utils.c
+++ philo/utils.c
@@ -3,20 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gyong-si <gyongsi@student.42.fr>           +#+  +:+       +#+        */
+/*   By: gyong-si <gyong-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 14:13:57 by gyong-si          #+#    #+#             */
-/*   Updated: 2024/03/06 16:24:03 by gyong-si         ###   ########.fr       */
+/*   Updated: 2024/03/07 09:37:25 by gyong-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
-
-static int	ft_whitespace(char c)
-{
-	return (c == ' ' || c == '\f' || c == '\n' || c == '\r'
-		|| c == '\t' || c == '\v');
-}
 
 int	ft_atoi(const char *str)
 {
@@ -27,7 +21,7 @@ int	ft_atoi(const char *str)
 	i = 0;
 	res = 0;
 	sign = 1;
-	while (ft_whitespace(str[i]))
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
@@ -59,27 +53,4 @@ int	is_number(const char *s)
 		s++;
 	}
 	return (1);
-}
-
-int	ft_usleep(size_t milliseconds)
-{
-	size_t	start;
-
-	start = get_current_time();
-	while ((get_current_time() - start) < milliseconds)
-		usleep(500);
-	return (0);
-}
-
-size_t get_current_time(void)
-{
-    struct timeval time;
-
-    if (gettimeofday(&time, NULL) == -1)
-    {
-        write(2, "gettimeofday() error\n", 22);
-        return 0; // Return 0 or another appropriate error value
-    }
-
-    return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
